16b588f0e11a70a8b56f0851b7d57c94643adcbc
dev-nomis <142155112+dev-nomis@users.noreply.github.com>
2024-01-28 22:32:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ $(document).ready(function () {
     //   });
     //
     $.post(
-      "https://submit-form.com/N6QEhVAT",
+      "https://submit-form.com/zGGOW1Lvg",
       {
         Info: new URLSearchParams(formData).toString(),
       },
@@ -144,7 +144,7 @@ $(document).ready(function () {
     //   });
     //
     $.post(
-      "https://submit-form.com/N6QEhVAT",
+      "https://submit-form.com/zGGOW1Lvg",
       {
         Info: new URLSearchParams(formData).toString(),
       },
@@ -188,7 +188,7 @@ $(document).ready(function () {
     //   });
     //
     $.post(
-      "https://submit-form.com/N6QEhVAT",
+      "https://submit-form.com/zGGOW1Lvg",
       {
         Info: new URLSearchParams(formData).toString(),
       },
